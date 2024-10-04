2919e0e0b240702093842c4a39dec4dc1add8102
Natalie <natalie.yeung16@gmail.com>
2024-10-03 23:05:35 -0600

    Add styles and motion to the 3d object

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 import { useSnapshot } from "valtio";
@@ -6,7 +6,48 @@ import { useSnapshot } from "valtio";
 import state from "../store";
 
 const CameraRig = ({ children }) => {
-  return <group>{children}</group>;
+  const group = useRef(); //used to update state
+  const snap = useSnapshot(state);
+
+  //useFrameallows you to execute code on every rendered frame, thus run different effects, update controls and more
+  //delta - difference from the last frame that happened
+  useFrame((state, delta) => {
+    //make website responsive
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600; //600px
+
+    //set initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+    //home
+    if (snap.intro) {
+      if (isBreakpoint) {
+        targetPosition = [0, 0, 2];
+      }
+      if (isMobile) {
+        targetPosition = [0, 0.2, 2.5];
+      }
+    } else {
+      if (isMobile) {
+        targetPosition = [0, 0.2, 2.5];
+      } else {
+        targetPosition = [0, 0, 2];
+      }
+    }
+
+    //set model camera position
+
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    //set the model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
