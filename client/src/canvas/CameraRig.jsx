a3e9f5e73aa2befa0a14994ed121e9c75934ac04
Natalie <natalie.yeung16@gmail.com>
2024-10-03 22:21:06 -0600

    Add 3D visual to the screen

---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+const CameraRig = ({ children }) => {
+  return <group>{children}</group>;
+};
+
+export default CameraRig;
