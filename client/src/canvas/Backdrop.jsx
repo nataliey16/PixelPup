bccc50bc5da2dedbae2b577dc660ed5e9a4d063c
Natalie <natalie.yeung16@gmail.com>
2024-10-04 15:42:22 -0600

    Add styles to object and shadow and lighting

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
+import { easing } from "maath";
+import { useFrame } from "@react-three/fiber";
+import { useGLTF } from "@react-three/drei";
+import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
-  return <div>Backdrop</div>;
+  const shadows = useRef();
+
+  return (
+    <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      <RandomizedLight
+        amount={4} // Increase the number of lights
+        radius={5}
+        intensity={2} // Boost intensity to brighten things up
+        ambient={0.5}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={1.2}
+        ambient={0.5}
+        position={[-5, 5, -9]}
+      />
+    </AccumulativeShadows>
+  );
 };
 
 export default Backdrop;
--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -14,15 +14,44 @@ const Shift = () => {
   const logoTexture = useTexture(snap.logoDecal);
   const fullTexture = useTexture(snap.fullDecal); //entire shirt
 
+  useFrame((state, delta) =>
+    easing.dampC(materials.lambert1.color, snap.color, 0.25, delta)
+  );
+
+  // create a string of state to keeps track of state changes and render the change
+  const stateString = JSON.stringify(snap);
+
   return (
-    <group>
+    <group key={stateString}>
       <mesh
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-      ></mesh>
+      >
+        {/* if full texture or full shirt */}
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+        {/* if logo */}
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0, 0.04, 0.15]}
+            rotation={[0, 0, 0]}
+            scale={0.15}
+            map={logoTexture}
+            anisotropy={16}
+            depthTest={false}
+            depthWrite={true}
+          />
+        )}
+      </mesh>
     </group>
   );
 };
