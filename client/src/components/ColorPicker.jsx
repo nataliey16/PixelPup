feedc0f7fb66426c5865a6d6865bfe95d68e77bf
Natalie <natalie.yeung16@gmail.com>
2024-10-07 09:35:52 -0600

    Implement SketchPicker library to customize object colour

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { SketchPicker } from "react-color";
+
+import { useSnapshot } from "valtio";
+
+import state from "../store";
 
 const ColorPicker = () => {
-  return <div>ColorPicker</div>;
+  const snap = useSnapshot(state);
+  return (
+    <div className="absolute left-full ml-3">
+      <SketchPicker
+        color={snap.color}
+        disableAlpha
+        onChange={(color) => (state.color = color.hex)}
+        // presentColors={[//add colors if you want]}
+      />
+    </div>
+  );
 };
 
 export default ColorPicker;
